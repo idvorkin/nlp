a608a3703a6278a42995cb64d0fa1fb827433fd2
Igor Dvorkin <idvorkin@gmail.com>
2023-04-17 20:57:46 +0000

    Add commands for compressing and uncompressing text using GPT-4
    The commit adds two new commands to the gpt3.py script, `compress` and `uncompress`, which allow users to compress and uncompress text using GPT-4. The `compress` command takes user input and compresses it as short as possible using GPT-4's language model, while the `uncompress` command takes compressed text and uses GPT-4 to reconstruct the original text without emojis for human consumption. The commit also includes several function calls for the new commands.

--- gpt3.py
+++ gpt3.py
@@ -94,19 +94,40 @@ def compress(
     responses: int = typer.Option(1),
     to_fzf: bool = typer.Option(False),
     debug: bool = typer.Option(False),
-    prompt: str = typer.Option("*"),
     stream: bool = typer.Option(True),
     u4: bool = typer.Option(True),
 ):
     user_text = remove_trailing_spaces("".join(sys.stdin.readlines()))
     # Find this prompt from the web
-    gpt_start_with = """Compress the following text as short as possible and such that you (GPT-4) can reconstruct the intention of the human
-    who wrote text as close as possible to the original intention.
-    This is for yourself. It does not need to be human readable or understandable.
-    Abuse of language mixing, abbreviations, symbols (unicode and emoji), or any other encodings or internal representations is all permissible, as long as it, if pasted in a new inference cycle, will yield near-identical results as the original text:
+    gpt_start_with = ""
+    prompt_to_gpt = (
+        """Compress the text following the colon in a way that it uses 30 percent less tokens such that you (GPT-4) can reconstruct the intention of the human who wrote text as close as possible to the original intention. This is for yourself. It does not need to be human readable or understandable. Abuse of language mixing, abbreviations, symbols (unicode and emoji), or any other encodings or internal representations is all permissible, as long as it, if pasted in a new inference cycle, will yield near-identical results as the original text (skip links and formatting):
 
     """
-    prompt_to_gpt = prompt.replace("*", user_text)
+        + user_text
+    )
+
+    base_query_from_dict(locals())
+
+
+@app.command()
+def uncompress(
+    tokens: int = typer.Option(0),
+    responses: int = typer.Option(1),
+    to_fzf: bool = typer.Option(False),
+    debug: bool = typer.Option(False),
+    stream: bool = typer.Option(True),
+    u4: bool = typer.Option(True),
+):
+    user_text = remove_trailing_spaces("".join(sys.stdin.readlines()))
+    # Find this prompt from the web
+    gpt_start_with = ""
+    prompt_to_gpt = (
+        """ You (GPT-4) Compressed the  following text in a way that it uses 30 percent less tokens,  reconstruct the original text without emojis for human consumption :
+
+    """
+        + user_text
+    )
 
     base_query_from_dict(locals())
 
