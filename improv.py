672625c4264f5481244cdbfc2971819c2f465e4f
Igor Dvorkin <idvorkin@gmail.com>
2023-05-29 08:35:36 -0700

    Clean up code a bit!

--- improv.py
+++ improv.py
@@ -15,8 +15,8 @@ from typeguard import typechecked
 import tiktoken
 import time
 from typing import List
-
 import signal
+import ast
 
 console = Console()
 
@@ -40,13 +40,6 @@ text_model_best = "gpt-3.5-turbo"
 code_model_best = "code-davinci-003"
 
 
-def bold_console(s):
-    if is_from_console:
-        return f"[bold]{s}[/bold]"
-    else:
-        return s
-
-
 # Load your API key from an environment variable or secret management service
 
 
@@ -63,11 +56,6 @@ gpt3 = setup_gpt()
 app = typer.Typer()
 
 
-# GPT performs poorly with trailing spaces (wow this function was writting by gpt)
-def remove_trailing_spaces(str):
-    return re.sub(r"\s+$", "", str)
-
-
 def num_tokens_from_string(string: str, encoding_name: str = "") -> int:
     """Returns the number of tokens in a text string."""
     encoding = tiktoken.get_encoding(encoding_name)
@@ -75,7 +63,7 @@ def num_tokens_from_string(string: str, encoding_name: str = "") -> int:
     return num_tokens
 
 
-def query_no_print(
+def ask_gpt(
     prompt_to_gpt="Make a rhyme about Dr. Seuss forgetting to pass a default paramater",
     tokens: int = 0,
     u4=True,
@@ -141,15 +129,12 @@ class Fragment:
 
     def __str__(self):
         if self.reasoning:
-            return f"Fragment('{self.player}', '{self.text}', '{self.reasoning}')"
+            return f'Fragment("{self.player}", "{self.text}", "{self.reasoning}")'
         else:
-            return f"Fragment('{self.player}', '{self.text}')"
+            return f'Fragment("{self.player}", "{self.text}")'
 
     def __repr__(self):
-        if self.reasoning:
-            return f"Fragment('{self.player}', '{self.text}', '{self.reasoning}')"
-        else:
-            return f"Fragment('{self.player}', '{self.text}')"
+        return str(self)
 
 
 def print_story(story: List[Fragment], show_story: bool):
@@ -196,19 +181,30 @@ def print_story(story: List[Fragment], show_story: bool):
 
 
 example_1_in = [
-    Fragment("coach", "Once Upon a Time", "A normal story start"),
+    Fragment("coach", "Once upon a time", "A normal story start"),
     Fragment("student", "there lived "),
     Fragment("coach", "a shrew named", "using shrew to make it intereting"),
     Fragment("student", "Sarah. Every day the shrew"),
 ]
+example_1_out = example_1_in + [
+    Fragment("coach", "smelled something that reminded her ", "give user a good offer")
+]
 
-default_story_start = [
-    Fragment("coach", "Once Upon a Time", "A normal story start"),
-    Fragment("student", "there lived "),
+example_2_in = [
+    Fragment(
+        "coach", "Once Upon a Time within ", "A normal story start, with a narrowing"
+    ),
+    Fragment("student", "there lived a donkey"),
+    Fragment("coach", "who liked to eat", "add some color"),
+    Fragment("student", "Brocolli. Every"),
+]
+
+example_2_out = example_2_in + [
+    Fragment("coach", "day the donkey", "continue in the format"),
 ]
 
 
-def prompt_json_improv(story_so_far: List[Fragment]):
+def ask_gpt_to_append_fragment_written_by_coach(story_so_far: List[Fragment]):
     return f"""
 You are a professional improv performer and coach. Help me improve my improv skills through doing practice.
 We're playing a game where we write a story together.
@@ -227,49 +223,26 @@ I'll write 1-5 words, and then you do the same, and we'll go back and forth writ
 The story is expressed as a json, I will pass in json, and you add the coach line to the json.
 You will add a third field as to why you added those words in the line
 Only add a single coach field to the output
+You can correct spelling and capilization mistakes
 The below strings are python strings, so if using ' quotes, ensure to escape them properly
 
 Example 1 Input:
 
-{
-    [
-     Fragment("coach", "Once Upon a Time", "A normal story start"),
-     Fragment("student", "there lived "),
-     Fragment("coach", "a shrew named", "using shrew to make it intereting"),
-     Fragment("student", "Sarah. Every day the shrew"),
-    ]
-}
+{example_1_in}
 
 Example 1 Output:
 
-[
-    Fragment("coach", "Once Upon a Time", "A normal story start"),
-    Fragment("student", "there lived "),
-    Fragment("coach", "a shrew named", "using shrew to make it intereting"),
-    Fragment("student", "Sarah. Every day the shrew"),
-    Fragment("coach", "smelled something that reminded her ", "give user a good offer"),
-]
-
+{example_1_out}
 --
 
 Example 2 Input:
 
-[
-    Fragment("coach", "Once Upon a Time within ", "A normal story start, with a narrowing"),
-    Fragment("student", "there lived a donkey"),
-    Fragment("coach", "who liked to eat", "add some color"),
-    Fragment("student", "Brocolli. Every" ),
-]
+{example_2_in}
 
 Example 2 Output:
 
-[
-    Fragment("coach", "Once Upon a Time within ", "A normal story start, with a narrowing"),
-    Fragment("student", "there lived a donkey"),
-    Fragment("coach", "who liked to eat", "add some color"),
-    Fragment("student", "Brocolli. Every" ),
-    Fragment("coach", "day the donkey", "continue in the format"),
-]
+{example_2_out}
+
 --
 
 Now, here is the story we're doing together. Add the next coach fragment to the story
@@ -283,26 +256,45 @@ Ouptut:
 """
 
 
+def safe_eval_of_fragment_list(fragment_list_as_text: str) -> List[Fragment]:
+
+    # input is valid python to construct fragments from text...
+    # [ Fragment(..),Fragment(..)]
+    # we don't want to run eval on that as it can run arbitrary stuff
+    # we cna remove the word fragment to make it a list of tuples
+    # then rebuild the tuples into fragments
+
+    fragment_list_as_text = fragment_list_as_text.replace("Fragment", "")
+    fragment_list = ast.literal_eval(fragment_list_as_text)
+    fragments = [Fragment(*tuple) for tuple in fragment_list]
+    return fragments
+
+
 @app.command()
-def with_json(
+def with_code(
     debug: bool = typer.Option(False),
     u4: bool = typer.Option(False),
 ):
 
+    default_story_start = [
+        Fragment("coach", "Once upon a time", "A normal story start"),
+    ]
     story = default_story_start
 
     while True:
-        the_story_as_text = query_no_print(
-            prompt_to_gpt=prompt_json_improv(story), debug=debug, u4=u4
-        )[0]
-        ic(the_story_as_text)
-
-        # Super risky, passing output of gpt directly to eval
-        story = eval(the_story_as_text)
         print_story(story, show_story=True)
         console.print("[yellow] >>[/yellow]", end="")
         user_says = input()
         story += [Fragment("student", user_says)]
+        valid_python_code_for_list_of_fragments = ask_gpt(
+            prompt_to_gpt=ask_gpt_to_append_fragment_written_by_coach(story),
+            debug=debug,
+            u4=u4,
+        )[0]
+        ic(valid_python_code_for_list_of_fragments)
+
+        #
+        story = safe_eval_of_fragment_list(valid_python_code_for_list_of_fragments)
 
 
 @app.command()
@@ -341,7 +333,7 @@ now add your words to the story (NEVER ADD MORE THEN 5 WORDS):
     while True:
         if debug:
             ic(prompt)
-        coach_says = query_no_print(prompt_to_gpt=prompt, debug=debug, u4=u4)[0]
+        coach_says = ask_gpt(prompt_to_gpt=prompt, debug=debug, u4=u4)[0]
         story += [Fragment("coach", coach_says)]
         prompt += coach_says
         print_story(story, show_story=False)
