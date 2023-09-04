9cdf91dbe825ec338951002a1569277435d4d2a3
Igor Dvorkin <idvorkin@gmail.com>
2023-09-04 00:25:03 +0000

    Checkpoint

--- setup.py
+++ setup.py
@@ -3,7 +3,14 @@ from setuptools import setup
 setup(
     name="idvorkin_nlp",
     version="0.1.0",
-    py_modules=["gpt3", "igor_journal", "dump_grateful", "improv", "openai_wrapper"],
+    py_modules=[
+        "gpt3",
+        "life",
+        "igor_journal",
+        "dump_grateful",
+        "improv",
+        "openai_wrapper",
+    ],
     install_requires=[],
     entry_points="""
             [console_scripts]
@@ -12,5 +19,6 @@ setup(
             ij=igor_journal:app
             dg=dump_grateful:app
             improv=improv:app
+            life=life:app
         """,
 )
