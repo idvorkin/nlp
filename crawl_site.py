4065f745e9e3662a613748a7520fd4012828cba3
Igor Dvorkin <idvorkin@gmail.com>
2023-12-26 07:55:13 -0800

    Add a crawler for what i leanred so far

--- crawl_site.py
+++ crawl_site.py
@@ -14,22 +14,15 @@ from bs4 import BeautifulSoup
 from icecream import ic
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
-from scrapy.item import Item, Field
 from scrapy import cmdline
 import html2text
 from scrapy import signals
-
-app = typer.Typer()
+from pathlib import Path
 
 site = "whatilearnedsofar.com"
-crawled_artciles = set()
+crawled_articles = set()
 glossary: Dict[str, str] = {}
-
-
-class MyItems(Item):
-    referer = Field()  # where the link is extracted
-    response = Field()  # url that was requested
-    status = Field()  # status code received
+output_root = Path().home() / "tmp/whatilearnedsofar"
 
 
 def process_page(response):
@@ -44,6 +37,7 @@ def process_page(response):
     # convert article to markdown
     converter = html2text.HTML2Text()
     converter.ignore_links = True
+    converter.body_width = 0  # no wrapping
     article_md = converter.handle(str(article))
 
     def remove_usage_from_glossary(article_md):
@@ -55,7 +49,7 @@ def process_page(response):
 
     article_md = remove_usage_from_glossary(article_md)
 
-    if article_md in crawled_artciles:
+    if article_md in crawled_articles:
         return
 
     if good_path.startswith("/glossary"):
@@ -64,87 +58,49 @@ def process_page(response):
         key = lines[0].replace("# ", "")
         value = "\n".join(lines[1:])
         glossary[key] = value
-        return
+        yield
 
-    crawled_artciles.add(article_md)
+    crawled_articles.add(article_md)
     ic(good_path)
     ic(article_md)
 
-    # list of response codes that we want to include on the report, we know
-
-    # that 404
-    report_if = [404]
-    if response.status in report_if:  # if the response matches then creates a MyItem
-        item = MyItems()
-        item["referer"] = response.request.headers.get("Referer", None)
-        item["status"] = response.status
-        item["response"] = response.url
-        yield item
-    yield None  # if the response did not match return emptyo
+    # write the file to disk
+    output_path = output_root / (good_path[1:-1] + ".md")
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+    output_path.write_text(article_md)
+    yield
 
 
 class MySpider(CrawlSpider):
-    name = "test-crawler"
-    site = "whatilearnedsofar.com"
-    target_domains = [site]  # list of domains that will be allowed to be crawled
+    name = "whatilearnedsofar"
     start_urls = [f"https://{site}"]  # list of starting urls for the crawler
-    handle_httpstatus_list = [
-        404,
-        410,
-        500,
-    ]  # only 200 by default. you can add more status to list
 
     @classmethod
     def from_crawler(cls, crawler, *args, **kwargs):
         spider = super().from_crawler(crawler, *args, **kwargs)
-        crawler.signals.connect(spider.spider_closed, signal=signals.spider_closed)
+        crawler.signals.connect(crawl_complete, signal=signals.spider_closed)
         return spider
-
-    # Throttle crawl speed to prevent hitting site too hard
-    custom_settings = {
-        "CONCURRENT_REQUESTS": 20,  # Some requests timeout, so have plenty of threads.
-        "DOWNLOAD_DELAY": 0.05,  # delay between requests
-        "REDIRECT_ENABLED": True,
-        "RETRY_ENABLED": False,
-    }
 
     rules = [
         Rule(
             LinkExtractor(
-                allow_domains=target_domains,
-                deny=("patterToBeExcluded"),
-                unique=("Yes"),
+                allow_domains=[site],
+                unique=True,
             ),
             callback=process_page,
             follow=True,
         ),
-        # crawl external links but don't follow them
-        # I don't follow what don't follow means - seems like it's actually crawling
-        # Rule(
-        # LinkExtractor(allow=(""), deny=("patterToBeExcluded"), unique=("Yes")),
-        # callback=parse_my_url,
-        # follow=False,
-        # ),
     ]
 
-    def spider_closed(self, spider):
-        print("spider closed")
-        ic(glossary)
+
+def crawl_complete(spider):  # noqa
+    ic(glossary)
+    ic(spider)
+    glossary_path = output_root / "glossary.md"
+    glossary_path.write_text("\n".join([f"## {k}\n{v}" for k, v in glossary.items()]))
 
 
-@app.command()
-def un():
-    from unstructured.partition.html import partition_html
-    from unstructured.chunking.title import chunk_by_title
-
-    url = "https://idvork.in/manager-book"
-    elements = partition_html(url=url)
-    chunks = chunk_by_title(elements)
-
-    for chunk in chunks:
-        print(chunk)
-        print("\n\n" + "-" * 80)
-        input()
+app = typer.Typer()
 
 
 @app.command()
