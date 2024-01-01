0d90dc82142d151b852ce44d946977c7300cbc69
Igor Dvorkin <idvorkin@gmail.com>
2024-01-01 07:26:34 -0800

    Add pbf

--- pbf.py
+++ pbf.py
@@ -5,8 +5,7 @@ from icecream import ic
 from loguru import logger
 from rich.console import Console
 from pathlib import Path
-from AppKit import NSPasteboard, NSFilenamesPboardType
-import objc
+from AppKit import NSPasteboard, NSFilenamesPboardType, NSArray
 
 
 console = Console()
@@ -14,23 +13,32 @@ app = typer.Typer()
 
 
 @app.command()
-def copy_to_clipboard(file: Path):
+def copy(file: Path):
     """Copy file to clipboard only on macos"""
 
     if not file.exists():
         ic("File does not exist")
         return
-    # copy file to clipboard
-    # a = NSArray.arrayWithObject_("hello world")
-    # pb.writeObjects_(a)
     pb = NSPasteboard.generalPasteboard()
     pb.clearContents()
     file_path = str(file.resolve())
     ic(file_path)
-    array = objc.lookUpClass("NSArray").arrayWithObject_(file_path)
+    array = NSArray.arrayWithObject_(file_path)
     pb.declareTypes_owner_([NSFilenamesPboardType], None)
     pb.setPropertyList_forType_(array, NSFilenamesPboardType)
     pb.writeObjects_(array)
+    ic(pb.types())
+
+
+def dump():
+    pb = NSPasteboard.generalPasteboard()
+    # Loop through all types and print out their corresponding content
+    types = pb.types()
+    for pboard_type in types:
+        content = pb.stringForType_(pboard_type)
+        ic(f"Content for type {pboard_type}:", content)
+    types = pb.types()
+    ic("Available types:", types)
 
 
 @logger.catch()
