5e470fe74edd19b7a922b66c5699458d9e8006a7
Igor Dvorkin <idvorkin@gmail.com>
2024-02-09 16:12:36 +0000

    Adding missing file

---
+++ discord_helper.py
@@ -0,0 +1,62 @@
+from typing import TypeVar, Generic, Callable
+from icecream import ic
+import asyncio
+
+T = TypeVar("T")
+
+
+class BotState(Generic[T]):
+    context_to_state = dict()
+    defaultStateFactory: Callable[[], T]
+
+    def __init__(self, defaultStateFactory: Callable[[], T]):
+        self.defaultStateFactory = defaultStateFactory
+
+    def __ket_for_ctx(self, ctx):
+        ic(type(ctx))
+        is_channel = ctx.guild is not None
+        if is_channel:
+            return f"{ctx.guild.name}-{ctx.channel.name}"
+        else:
+            return f"DM-{ctx.author.name}-{ctx.author.id}"
+
+    def get(self, ctx) -> T:
+        key = self.__ket_for_ctx(ctx)
+        if key not in self.context_to_state:
+            self.reset(ctx)
+
+        # return a copy of the story
+        return self.context_to_state[key]
+
+    def set(self, ctx, state: T):
+        key = self.__ket_for_ctx(ctx)
+        ic("setting state", key)
+        self.context_to_state[key] = state
+
+    def reset(self, ctx):
+        self.set(ctx, self.defaultStateFactory())
+        ic("bot reset")
+
+
+def ctx_to_send_function(ctx):
+    is_channel = hasattr(ctx, "channel")
+    return ctx.channel.send if is_channel else ctx.send
+
+
+async def send(ctx, message):
+    return await ctx_to_send_function(ctx)(message)
+
+
+async def draw_progress_bar(ctx):
+    progress_message = await send(ctx, ".")
+    return asyncio.create_task(
+        edit_message_to_append_dots_every_second(progress_message, ".")
+    )
+
+
+async def edit_message_to_append_dots_every_second(message, base_text):
+    # Stop after 30 seconds - probably nver gonna come back after that.
+    for _ in range(30 * 2):
+        base_text += "."
+        await message.edit(base_text)
+        await asyncio.sleep(0.5)
