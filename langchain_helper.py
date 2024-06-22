8d459b888ef486a839029371f67a91e054fe45fd
Igor Dvorkin <idvorkin@gmail.com>
2024-06-22 08:02:39 -0700

    Upgrade to soonnet

--- langchain_helper.py
+++ langchain_helper.py
@@ -38,7 +38,7 @@ def get_models(
     if claude:
         from langchain_anthropic import ChatAnthropic
 
-        model = ChatAnthropic(model_name="claude-3-opus-20240229")
+        model = ChatAnthropic(model_name="claude-3-5-sonnet-20240620")
         ret.append(model)
 
     if llama:
