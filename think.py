4474e4cb9488b2a1d7632c590bf4c9b1ed8c988e
Igor Dvorkin <idvorkin@gmail.com>
2024-08-11 12:03:16 -0700

    Update max token size

--- think.py
+++ think.py
@@ -170,12 +170,8 @@ async def a_think(gist: bool, writer: bool, path: str, core_problems: bool):
     user_text = langchain_helper.get_text_from_path_or_stdin(path)
     tokens = num_tokens_from_string(user_text)
 
-    if tokens < 8000:
+    if tokens < 60_000:  # Groq limits to 60K ish
         # only add Llama if the text is small
-        llms += [langchain_helper.get_model(llama=True)]
-
-    if tokens < 4000:
-        # Add it twice if it fits
         llms += [langchain_helper.get_model(llama=True)]
 
     categories = AnalysisQuestions.default()
