2912587b3b4f438642438ec3e577b3320efd4ca2
Igor Dvorkin <idvorkin@gmail.com>
2024-09-22 10:59:13 -0700

    Add support for studio

--- pell.py
+++ pell.py
@@ -1,17 +1,25 @@
 #!python3
+# pylint: disable=missing-function-docstring
+
 
 import typer
 from icecream import ic
 from rich.console import Console
 from loguru import logger
 import ell
+import os
 
 app = typer.Typer()
+
+# Define ELL_LOGDIR as a constant
+ELL_LOGDIR = os.path.expanduser("~/tmp/ell_logdir")
+
+ell.init(store=ELL_LOGDIR, autocommit=True)
 
 
 @ell.simple(model="gpt-4o-mini")
 def hello(world: str):
-    """You are a helpful assistant"""  # System prompt
+    """You are a unhelpful assistant"""  # System prompt
     name = world.capitalize()
     return f"Say hello to {name}!"  # User prompt
 
@@ -21,9 +29,16 @@ app = typer.Typer(no_args_is_help=True)
 
 
 @app.command()
-def simple_1():
-    response = hello("sam altman")  # just a str, "Hello Sam Altman! ..."
+def scratch():
+    response = hello("Igor", lm_parms=dict(n=2))
     ic(response)
+
+
+@app.command()
+def studio():
+    import subprocess
+
+    subprocess.run(["ell-studio", "--storage", ELL_LOGDIR])
 
 
 @logger.catch()
