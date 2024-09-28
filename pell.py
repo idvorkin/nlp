d12462a79507acb1c37f5d6d33668a8ba4a26472
Igor Dvorkin <idvorkin@gmail.com>
2024-09-28 14:52:12 -0700

    w00t! Groq works (doesn't have vision yet), and also better ergonomics on launching studio

--- pell.py
+++ pell.py
@@ -16,6 +16,13 @@ app = typer.Typer(no_args_is_help=True)
 ELL_LOGDIR = os.path.expanduser("~/tmp/ell_logdir")
 
 ell.init(store=ELL_LOGDIR, autocommit=True)
+ell.models.groq.register()
+
+
+@app.command()
+def list_models():
+    console.print("Models available:")
+    # console.print(ell.models.groq.
 
 
 @ell.simple(model="gpt-4o-mini")
@@ -25,17 +32,57 @@ def hello(world: str):
     return f"Say hello to {name}!"  # User prompt
 
 
+# @ell.simple(model="llama-3.2-90b-vision-preview")
+@ell.simple(model="llama-3.2-11b-vision-preview")
+def hello_groq(world: str):
+    """You are a unhelpful assistant, make your answers spicy"""  # System prompt
+    name = world.capitalize()
+    return f"Say hello to {name}!"  # User prompt
+
+
 @app.command()
 def scratch():
-    response = hello("Igor", lm_params=dict(n=2))
+    response = hello_groq("Igor", api_params=dict(n=1))
     ic(response)
 
 
 @app.command()
 def studio():
     import subprocess
+    import socket
+    import asyncio
 
-    subprocess.run(["ell-studio", "--storage", ELL_LOGDIR])
+    def is_port_in_use(port):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex(("127.0.0.1", port))
+        sock.close()
+        return result == 0
+
+    def open_browser():
+        subprocess.run(["open", "http://127.0.0.1:5555"])
+
+    if not is_port_in_use(5555):
+
+        async def run_server_and_open_browser():
+            # Start the server asynchronously
+            server_process = await asyncio.create_subprocess_exec(
+                "ell-studio", "--storage", ELL_LOGDIR
+            )
+
+            # Wait for 2 seconds
+            await asyncio.sleep(2)
+
+            # Open the browser
+            open_browser()
+
+            # Keep the server running
+            await server_process.wait()
+
+        # Run the async function
+        asyncio.run(run_server_and_open_browser())
+    else:
+        ic("Studio is already running")
+        open_browser()
 
 
 @logger.catch()
