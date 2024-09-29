164d4bb9895703974de1e95eb087b44786ef781c
Igor Dvorkin <idvorkin@gmail.com>
2024-09-29 08:28:59 -0700

    Port vapi_sim to ell (great simplification of tools calls!)

--- vapi_sim.py
+++ vapi_sim.py
@@ -4,15 +4,16 @@ import os
 
 import typer
 from icecream import ic
-from langchain_helper import get_model, get_model_name
-from langchain_community.chat_message_histories import ChatMessageHistory
-from langchain.prompts import ChatPromptTemplate
-from langchain_core.messages import SystemMessage, ToolMessage
 from loguru import logger
 from rich import print
 from rich.console import Console
 import requests
-from langchain_core.tools import StructuredTool, tool
+import ell
+from ell import Message
+import openai_wrapper
+
+ELL_LOGDIR = os.path.expanduser("~/tmp/ell_logdir")
+ell.init(store=ELL_LOGDIR, autocommit=True)
 
 
 DEFAULT_SEARCH_QUERY = "What's the weather in moscow"
@@ -21,26 +22,26 @@ console = Console()
 app = typer.Typer(no_args_is_help=True)
 
 
-@tool
-def journal_append(content):
+@ell.tool()
+def journal_append(content: str):
     """Append content to the journal"""
     return call_tony_server_as_vapi("journal-append", content=content)
 
 
-@tool
-def journal_read(date):
+@ell.tool()
+def journal_read(date: str):
     """Read  the journal"""
     return call_tony_server_as_vapi("journal-read", date=date)
 
 
-@tool
+@ell.tool()
 def library_arrivals():
     """When the bus gets to the library, which is the bus stop for garfield, when user asks when is the next bus to garfield"""
     return call_tony_server_as_vapi("library-arrivals")
 
 
-@tool
-def search(question):
+@ell.tool()
+def search(question: str):
     """Search the web"""
     return call_tony_server_as_vapi("search", question=question)
 
@@ -55,28 +56,17 @@ def call_tony_server_as_vapi(api, **kwargs):
     return str(response)
 
 
-def process_tool_calls(llm_result) -> list[ToolMessage]:
-    TONY_TOOLS_STRUCTURED_HACK: list[StructuredTool] = TONY_TOOLS  # type:ignore - TONY_TOOLS are actually StructuredTools once wrapped
-    return [
-        process_tool_call(TONY_TOOLS_STRUCTURED_HACK, tool_call)
-        for tool_call in llm_result.tool_calls
-    ]
-
-
 TONY_TOOLS = [journal_append, journal_read, search, library_arrivals]
 
 
-# TODO: Consider transforming this into a nice lookup table
-def process_tool_call(tools: list[StructuredTool], tool_call: dict):
-    tool_name = tool_call["name"]
-    get_name = lambda t: t.func.__name__ if t.func else "unknown"  # noqa - all functions should be valid
-
-    # find the function in tony's too list
-    tool = [t.func for t in tools if get_name(t) == tool_name][0]
-    assert tool  # there needs to be a matching tool
-    ic(tool_call["args"])
-    tool_ret = tool(**tool_call["args"])
-    return ToolMessage(tool_call_id=tool_call["id"], content=tool_ret)
+@ell.complex(model=openai_wrapper.get_ell_model(openai=True), tools=TONY_TOOLS)
+def prompt_to_llm(message_history: list[Message]):
+    return (
+        [
+            # the first message will be the system message
+        ]
+        + message_history
+    )
 
 
 def get_tony_server_url(dev_server: bool) -> str:
@@ -87,67 +77,60 @@ def get_tony_server_url(dev_server: bool) -> str:
         return "https://idvorkin--modal-tony-server-assistant.modal.run"
 
 
+# @ell.complex(model="gpt-4o-
+# def call_tony()
+
+
 @app.command()
 def tony(dev_server: bool = False):
     """Talk to Toni"""
 
-    # from langchain.chat_models import init_chat_model
-    # model = init_chat_model(model_name).
-    ic("v0.03")
-    ic("++init model")
-    model = get_model(openai=True).bind_tools(TONY_TOOLS)
-    ic(get_model_name(model))  # type: ignore
-    ic("--init model")
+    ic("v0.0.4")
 
     ic("++assistant.api")
     payload = {"ignored": "ignored"}
     url_tony = get_tony_server_url(dev_server)
     ic(url_tony)
-    tony_response = requests.post(url_tony, json=payload)
-    if tony_response.status_code != 200:
-        ic(tony_response)
+    assistant_response = requests.post(url_tony, json=payload)
+    if assistant_response.status_code != 200:
+        ic(assistant_response)
         return
 
-    model_from_assistant = tony_response.json()["assistant"]["model"]["model"]
+    model_from_assistant = assistant_response.json()["assistant"]["model"]["model"]
     ic(model_from_assistant)
     ic("--assistant.api")
 
-    memory = ChatMessageHistory()
+    messages = []
 
     # TODO build a program to parse this out
-    system_message_content = tony_response.json()["assistant"]["model"]["messages"][0][
-        "content"
-    ]
-    memory.add_message(SystemMessage(content=system_message_content))
+    system_message_content = assistant_response.json()["assistant"]["model"][
+        "messages"
+    ][0]["content"]
+    messages.append(ell.system(system_message_content))
 
     while True:
-        is_last_message_tool_response = isinstance(memory.messages[-1], ToolMessage)
+        # if there's a tool response, we need to call the model again
+        user_input = input("Igor:")
+        if user_input == "debug":
+            ic(model_from_assistant)
+            continue
+        if user_input == "search":
+            ic("hardcode test")
+            user_input = DEFAULT_SEARCH_QUERY
+        messages.append(ell.user(user_input))
+        # ic(custom_instructions)
 
-        if not is_last_message_tool_response:
-            # if there's a tool response, we need to call the model again
-            user_input = input("Igor:")
-            if user_input == "debug":
-                ic(model_from_assistant)
-                continue
-            if user_input == "search":
-                ic("hardcode test")
-                user_input = DEFAULT_SEARCH_QUERY
-            memory.add_user_message(message=user_input)
-            # ic(custom_instructions)
+        tony_response: Message | list[Message] = prompt_to_llm(messages)  # type: ignore
+        if tony_response.tool_calls:
+            messages.append(tony_response)
+            next_message = tony_response.call_tools_and_collect_as_message()
+            messages.append(next_message)
+            # call tony again
+            tony_response = prompt_to_llm(messages)
 
-        prompt = ChatPromptTemplate.from_messages(memory.messages)
-        chain = prompt | model
-        llm_result = chain.invoke({})  # type: ignore
-        memory.add_message(llm_result)
-        tool_responses = process_tool_calls(llm_result)
-        if len(tool_responses):
-            memory.add_messages(tool_responses)
-            prompt = ChatPromptTemplate.from_messages(memory.messages)
-            chain = prompt | model
-            llm_result = chain.invoke({})  # type: ignore
-            memory.add_message(llm_result)
+        messages.append(tony_response)
 
-        print(f"[yellow]Tony:{llm_result.content}")
+        print(f"[yellow]Tony:{tony_response.text}")
 
 
 @logger.catch()
