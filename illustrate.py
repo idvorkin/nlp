97df077bd2619751225b05cec4992c4e13c5beae
Igor Dvorkin <idvorkin@gmail.com>
2024-10-10 07:15:58 -0700

    Migrate illustrate to ell

--- illustrate.py
+++ illustrate.py
@@ -1,49 +1,46 @@
 #!python3
 
-
-import asyncio
-from langchain.schema.output_parser import StrOutputParser
-
-from langchain_core import messages
+import sys
 import typer
-from langchain.prompts import ChatPromptTemplate
 from loguru import logger
 from rich import print
 from rich.console import Console
-import langchain_helper
+import openai_wrapper
+import ell
+import os
 
+# Define ELL_LOGDIR as a constant
+ELL_LOGDIR = os.path.expanduser("~/tmp/ell_logdir")
 
-def prompt_illustrate(content):
-    instructions = """
-Help me create an image to represent this blog post, by making some prompts for GPT. They should be fun whimsical and if they feature peope but as 3d render of a cartoon racoon in the pixar style, the main raccon should be in his late 30s wearing very colorful glasses, and whimsical. End the prompt asking to render each photo, not just  the first one
-"""
-    return ChatPromptTemplate.from_messages(
-        [
-            messages.SystemMessage(content=instructions),
-            messages.HumanMessage(content=content),
-        ]
-    )
-
-
-async def a_draw(path: str):
-    llm = langchain_helper.get_model(openai=True)
-    user_text = langchain_helper.get_text_from_path_or_stdin(path)
-    ret = (prompt_illustrate(user_text) | llm | StrOutputParser()).invoke({})
-    print(ret)
-
+ell.init(store=ELL_LOGDIR, autocommit=True)
 
 console = Console()
 app = typer.Typer(no_args_is_help=True)
 
 
+@ell.simple(model=openai_wrapper.gpt4.name)
+def prompt_illustrate(content: str):
+    """
+    Help me create an image to represent this blog post, by making some prompts for GPT. They should be fun whimsical and if they feature people but as 3d render of a cartoon racoon in the pixar style, the main racoon should be in his late 30s wearing very colorful glasses, and whimsical. End the prompt asking to render each photo, not just the first one
+    """
+    return content  # This will be the user prompt
+
+
+def get_text_from_path_or_stdin(path: str) -> str:
+    if path:
+        with open(path, "r") as file:
+            return file.read()
+    else:
+        return "".join(sys.stdin.readlines())
+
+
 @app.command()
 def draw(
-    trace: bool = False,
     path: str = typer.Argument(None),
 ):
-    langchain_helper.langsmith_trace_if_requested(
-        trace, lambda: asyncio.run(a_draw(path))
-    )
+    user_text = get_text_from_path_or_stdin(path)
+    ret = prompt_illustrate(user_text)
+    print(ret)
 
 
 @logger.catch()
